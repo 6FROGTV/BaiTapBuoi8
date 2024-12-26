2ea20640d977b243f1907bc805ebe1a1901f5029
6FROGTV <anhhuybui672@gmail.com>
2024-12-26 19:02:00 +0700

    first commit

---
+++ BUS/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Model1;
+
+namespace BUS
+{
+    public class Class1
+    {
+        private Model1 _context;
+
+        // Dịch phương thức khởi tạo
+        public ProductRepository(Model1 context)
+        {
+            _context = context;
+        }
+
+        // Lấy danh sách tất cả sản phẩm
+        public List<Sanpham> LayTatCaSanPham()
+        {
+            return _context.Sanpham.Include(s => s.LoaiSP).ToList();
+        }
+
+        // Lấy sản phẩm theo mã sản phẩm
+        public Sanpham LaySanPhamTheoMa(string maSP)
+        {
+            return _context.Sanpham.FirstOrDefault(p => p.MaSP == maSP);
+        }
+
+        // Tạo sản phẩm mới
+        public void TaoSanPham(Sanpham product)
+        {
+            _context.Sanpham.Add(product);
+            _context.SaveChanges();
+        }
+
+        // Cập nhật sản phẩm
+        public void CapNhatSanPham(Sanpham product)
+        {
+            _context.Entry(product).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        
---
+++ DAL/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Model1;
+
+namespace DAL
+{
+    public class Class1
+    {
+        private readonly ProductRepository _productRepository;
+
+        // Dịch phương thức khởi tạo
+        public ProductService(ProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        // Lấy danh sách sản phẩm
+        public List<Sanpham> LayDanhSachSanPham()
+        {
+            return _productRepository.GetAllProducts();
+        }
+
+        // Tạo sản phẩm mới
+        public void TaoSanPham(Sanpham product)
+        {
+            // Thêm logic kiểm tra dữ liệu đầu vào ở đây (ví dụ: kiểm tra mã sản phẩm đã tồn tại chưa)
+            _productRepository.CreateProduct(product);
+        }
+
+        // Cập nhật sản phẩm
+        public void CapNhatSanPham(Sanpham product)
+        {
+            // Thêm logic kiểm tra dữ liệu đầu vào ở đây
+            _productRepository.UpdateProduct(product);
+        }
+
+        // Xóa sản phẩm
+        public void XoaSanPham(string maSP)
+        {
+            var product = _productRepository.GetProductById(maSP);
+            if (product != null)
+            {
+                _productRepository.DeleteProduct(product);
+            }
+        }
+    }
+}
